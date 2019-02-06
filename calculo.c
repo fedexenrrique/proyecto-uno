5d889ccef5adf727d240f2b0efeeb1a387cc0c22
Federico A. Enrrique <fede.enrrique@gmail.com>
2019-02-06 16:39:31 -0300

    'subida a git por fede desde consola.'

---
+++ calculo.c
@@ -0,0 +1,15 @@
+/*
+ * calculo.c
+ *
+ *  Created on: 6 feb. 2019
+ *      Author: utnso
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void) {
+	puts("¡¡Hello World!!");
+	return 0;
+}
+
